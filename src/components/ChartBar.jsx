35b3344d02c334f0f5c817acadd09aad1a527355
SanchezCamille <camille.sanchez@oclock.school>
2024-01-09 16:18:25 +0100

    adding colors to max value chart bar

--- src/components/ChartBar.jsx
+++ src/components/ChartBar.jsx
@@ -1,20 +1,40 @@
-import { BarChart, Bar, XAxis, Tooltip } from 'recharts';
-import './ChartBar.scss';
+/* eslint-disable react/prop-types */
+
+import { BarChart, Bar, Cell } from 'recharts';
 import Data from '../../data.json';
 
-function ChartBar() {
+function RoundedBar(props) {
+  const { x, y, width, height, fill } = props;
+
   return (
-    <>
-      <BarChart width={400} height={200} data={Data}>
-        <XAxis dataKey="day" hide={true}/>
-        <Tooltip />
-        <Bar dataKey="amount" fill="hsl(10, 79%, 65%)" ClassName="bar" />
-      </BarChart>
-    </>
+    <g>
+      <rect x={x} y={y} width={width} height={height} fill={fill} rx={4} />
+    </g>
+  );
+}
+
+function ChartBar() {
+
+  const maxValue = Data.reduce((max, item) => {
+    return item.amount > max ? item.amount : max;
+  }, Number.MIN_SAFE_INTEGER);
+
+  return (
+    
+    <BarChart width={400} height={300} data={Data} >
+        <Bar dataKey="amount" shape={RoundedBar}>
+          {Data.map((entry, index) => (
+            <Cell
+              key={`cell-${index}`}
+              cursor="pointer"
+              fill={entry.amount === maxValue ? 'hsl(186, 34%, 60%)' : 'hsl(10, 79%, 65%)'}
+            />
+          ))}
+        </Bar>
+    </BarChart>
+    
   );
 }
 
 export default ChartBar;
-
-
 
